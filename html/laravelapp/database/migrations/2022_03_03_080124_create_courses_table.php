6698a6c5c88476c7920dc99ac6ec27d9cf69559e
Yuzunoha <studysteadystandby@gmail.com>
2022-03-04 17:03:15 +0900

    nullable

---
+++ html/laravelapp/database/migrations/2022_03_03_080124_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('courses', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->dateTime('created_at');
+      $table->dateTime('updated_at');
+      $table->dateTime('deleted_at')->nullable();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('courses');
+  }
+}
