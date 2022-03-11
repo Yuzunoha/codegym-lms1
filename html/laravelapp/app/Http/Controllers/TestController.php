e7086ab5c95c2817ab692ae187c702d6ee72ab58
Yuzunoha <studysteadystandby@gmail.com>
2022-03-12 00:06:12 +0900

    TestService->index()です。戻り値型指定しました。

--- html/laravelapp/app/Http/Controllers/TestController.php
+++ html/laravelapp/app/Http/Controllers/TestController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TestService;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
+  protected $testService;
+
+  public function __construct(TestService $testService)
+  {
+    $this->testService = $testService;
+  }
+
   public function index()
   {
-    return 'indexです';
+    return $this->testService->index();
   }
 }
