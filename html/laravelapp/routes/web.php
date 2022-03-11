8e99514dbd0d83a1603832f490fd86cd133d6ec5
Yuzunoha <studysteadystandby@gmail.com>
2022-03-11 23:04:53 +0900

    test

--- html/laravelapp/routes/web.php
+++ html/laravelapp/routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->name('dashboard');
+
+Route::get('test', function () {
+  return view('welcome');
+});
