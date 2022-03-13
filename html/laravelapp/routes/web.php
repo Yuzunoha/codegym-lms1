0935e02d04d338745045ac8dc10fbbe233966b74
Yuzunoha <studysteadystandby@gmail.com>
2022-03-13 22:54:57 +0900

    http://localhost/sample1

--- html/laravelapp/routes/web.php
+++ html/laravelapp/routes/web.php
@@ -23,6 +23,7 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 Route::get('test/arrow', fn () => "こんにちは!");
 Route::get('test', 'TestController@index');
-Route::get('adminlte', function () {
-  return view('adminlte');
+
+Route::get('sample1', function () {
+  return view('sample1');
 });
