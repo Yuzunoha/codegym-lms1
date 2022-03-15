e84bd95999face75b27f6763ccaec9ec2b3571c7
Yuzunoha <studysteadystandby@gmail.com>
2022-03-15 16:13:34 +0900

    これでいく

--- html/laravelapp/resources/views/courses/edit.blade.php
+++ html/laravelapp/resources/views/courses/edit.blade.php
@@ -39,6 +39,68 @@
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+<div class="container-fluid" style="margin-top: 50px;">
+  <h5>包含しているclazzes</h5>
+</div>
+<div class="card">
+  <div class="card-body p-0">
+    <table class="table table-bordered table-striped">
+      <thead>
+        <th>id</th>
+        <th>name</th>
+        <th>created_at</th>
+        <th>updated_at</th>
+        <th>deleted_at</th>
+      </thead>
+      <tr>
+        <td>{{ $course->id }}</td>
+        <td>{{ $course->name }}</td>
+        <td>{{ $course->created_at }}</td>
+        <td>{{ $course->updated_at }}</td>
+        <td>{{ $course->deleted_at }}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><input type="text" class="form-control" name="name" value="{{ $course->name }}"></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+</div>
+
+<div class="container-fluid" style="margin-top: 50px;">
+  <h5>包含していないclazzes</h5>
+</div>
+<div class="card">
+  <div class="card-body p-0">
+    <table class="table table-bordered table-striped">
+      <thead>
+        <th>id</th>
+        <th>name</th>
+        <th>created_at</th>
+        <th>updated_at</th>
+        <th>deleted_at</th>
+      </thead>
+      <tr>
+        <td>{{ $course->id }}</td>
+        <td>{{ $course->name }}</td>
+        <td>{{ $course->created_at }}</td>
+        <td>{{ $course->updated_at }}</td>
+        <td>{{ $course->deleted_at }}</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td><input type="text" class="form-control" name="name" value="{{ $course->name }}"></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+</div>
 @stop
 
 @section('js')
