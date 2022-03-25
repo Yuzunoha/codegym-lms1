48f4f845ec4bb95e470e898b594b8f38fcbe5267
Yuzunoha <studysteadystandby@gmail.com>
2022-03-25 15:24:30 +0900

    'logo' => '<b>CODEGYM</b> LMS',

--- html/laravelapp/config/adminlte.php
+++ html/laravelapp/config/adminlte.php
@@ -45,12 +45,12 @@ return [
     |
     */
 
-  'logo' => '<b>Admin</b>LTE',
+  'logo' => '<b>CODEGYM</b> LMS',
   'logo_img' => 'images/codegym_logo.png',
-  'logo_img_class' => 'brand-image img-circle elevation-3',
+  'logo_img_class' => 'brand-image img-circle',
   'logo_img_xl' => null,
   'logo_img_xl_class' => 'brand-image-xs',
-  'logo_img_alt' => 'AdminLTE',
+  'logo_img_alt' => 'codegym_logo.png',
 
   /*
     |--------------------------------------------------------------------------
